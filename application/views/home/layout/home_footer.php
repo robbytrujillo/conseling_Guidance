170ff6faf4de387f0233862e127f80dfce9db996
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-02-17 13:52:20 +0700

    SI ihbscg

--- application/views/home/layout/home_footer.php
+++ application/views/home/layout/home_footer.php
@@ -2,12 +2,12 @@
       <div class="container">
 
 
-        <div class="row pt-5 mt-5 text-center">
+        <div class="row pt-1 mt-1 text-center" >
           <div class="col-md-12">
-            <div class="border-top pt-5">
+            <div class="border-top pt-2">
             <p>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> IT <span style="color: darkolivegreen"><b>ihbs</b></span>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
       </p>
             </div>
